4dc0f779f9a150f372b56f54a7ab458aa0ae8327
signzy <nkashter@gmail.com>
2018-08-27 15:54:45 +0530

    Jersey setup

---
+++ src/main/java/com/nithin/jerseytest/Application.java
@@ -0,0 +1,39 @@
+package com.nithin.jerseytest;
+
+import com.nithin.jerseytest.EDController.Caesar;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        Server jettyServer = new Server(8080);
+        jettyServer.setHandler(context);
+
+        ServletHolder jerseyServlet = context.addServlet(
+                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
+
+        jerseyServlet.setInitParameter(
+                "jersey.config.server.provider.classnames", "com.nithin.jerseytest.test, com.nithin.jerseytest.EDController.Caesar");
+
+//        ServletHolder jerseyServletOne = context.addServlet(
+//                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+//        jerseyServletOne.setInitOrder(0);
+
+//        jerseyServletOne.setInitParameter(
+//                "jersey.config.server.provider.classnames",
+//                Caesar.class.getCanonicalName());
+
+        try {
+            jettyServer.start();
+            jettyServer.join();
+        } finally {
+            jettyServer.destroy();
+        }
+    }
+}
---
+++ src/main/java/com/nithin/jerseytest/EDController/Caesar.java
@@ -0,0 +1,23 @@
+package com.nithin.jerseytest.EDController;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("caesar")
+public class Caesar {
+
+    @POST
+    @Path("encrypt")
+    @Consumes("application/x-www-form-urlencoded")
+    public String encryption(@FormParam("message") String message, @FormParam("offset") int offset){
+        return com.nithin.jerseytest.EDHandler.Caesar.encrypt(message, offset);
+    }
+
+    @POST
+    @Path("decrypt")
+    @Consumes("application/x-www-form-urlencoded")
+    public String decryption(@FormParam("message") String message, @FormParam("offset") int offset){
+        return com.nithin.jerseytest.EDHandler.Caesar.decrypt(message, offset);
+    }
+
+}
